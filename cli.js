f6d61749be23e5d796346ceb633a2b887398f20c
leeandher <leander.rodrigues@accenture.com>
2018-10-10 09:39:22 -0400

    set up POC live twitter stream

--- cli.js
+++ cli.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 require("dotenv").config();
-const Twit = require("twit");
+const twit = require("twit");
+const chalk = require("chalk");
+const log = console.log;
 
-const T = new Twit({
+const T = new twit({
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
-  // timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  // strictSSL: true // optional - requires SSL certificates to be valid.
 });
 
-T.get("search/tweets", { q: "banana since:2011-07-11", count: 100 }, function(
-  err,
-  data,
-  response
-) {
-  console.log(data);
+const track = "lit";
+const stream = T.stream("statuses/filter", { track });
+
+stream.on("tweet", function(tweet) {
+  log(chalk.blue.bold(`${tweet.user.name} (@${tweet.user.screen_name})`));
+
+  log(`"${tweet.text}"`);
+  log("");
 });
