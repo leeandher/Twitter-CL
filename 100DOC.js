d9213eed31713ea5e05a0fabf75007addbb7de34
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-11-30 10:53:57 -0500

    testing and re org

--- 100DOC.js
+++ 100DOC.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
-require("dotenv").config();
+require("dotenv").config({ path: "./twitter-config.env" });
 const twit = require("twit");
 const chalk = require("chalk");
 const log = console.log;
 
 const T = new twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const templateTweet = `#100DaysOfCode Day 011: JS30 29, some 'Bi-directional Data Flow' through React, and got oriented with 'react-transition-group'. --Tweeted from CLI (https://github.com/leeandher/twitter-from-cli)`;
-T.post("statuses/update", { status: templateTweet }, function(data) {
-  console.log(data);
-});
+console.log;
+
+// const templateTweet = `#100DaysOfCode Day 011: JS30 29, some 'Bi-directional Data Flow' through React, and got oriented with 'react-transition-group'. --Tweeted from CLI (https://github.com/leeandher/twitter-from-cli)`;
+// T.post("statuses/update", { status: templateTweet }, function(data) {
+//   console.log(data);
+// });
 
 //To-do: dive into the twit package and disect, see if i can make my own functions
--- cli.js
+++ cli.js
@@ -5,10 +5,10 @@ const chalk = require("chalk");
 const log = console.log;
 
 const T = new twit({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 const track = "kanye";
