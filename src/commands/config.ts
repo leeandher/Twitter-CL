21323b34c76c92f4e63d3c884eb9a916837d08ac
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-01-14 18:17:48 -0500

    🚧 Setting up twitter client

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,101 +1,101 @@
-import { Command, flags } from "@oclif/command";
-import { prompt } from "inquirer";
-import * as fs from "fs";
-import chalk from "chalk";
+import {Command, flags} from '@oclif/command'
+import chalk from 'chalk'
+import * as fs from 'fs'
+import {prompt} from 'inquirer'
 
 export default class Config extends Command {
   static description = `Setup your account credentials.\n ${chalk.yellow(
-    "NOTE:"
-  )} This requires API keys from https://developer.twitter.com!`;
+    'NOTE:'
+  )} This requires API keys from https://developer.twitter.com!`
 
   static flags = {
-    help: flags.help({ char: "h" })
-  };
+    help: flags.help({char: 'h'})
+  }
 
   static configQuestions = [
     {
-      type: "input",
-      name: "consumer_key",
-      message: "Consumer API Key"
+      type: 'input',
+      name: 'consumer_key',
+      message: 'Consumer API Key'
     },
     {
-      type: "input",
-      name: "consumer_secret",
-      message: "Consumer API Secret Key"
+      type: 'input',
+      name: 'consumer_secret',
+      message: 'Consumer API Secret Key'
     },
     {
-      type: "input",
-      name: "access_token_key",
-      message: "Access Token Key"
+      type: 'input',
+      name: 'access_token_key',
+      message: 'Access Token Key'
     },
     {
-      type: "input",
-      name: "access_token_secret",
-      message: "Access Token Secret"
+      type: 'input',
+      name: 'access_token_secret',
+      message: 'Access Token Secret'
     }
-  ];
+  ]
 
   async run() {
     // Setup the user's configuration directory
-    this.log("Setting up your configuration directory...");
+    this.log('Setting up your configuration directory...')
     if (fs.existsSync(this.config.configDir)) {
-      this.log(`Using: ${chalk.cyan(this.config.configDir)}`);
+      this.log(`Using: ${chalk.cyan(this.config.configDir)}`)
     } else {
-      fs.mkdirSync(this.config.configDir);
-      this.log(`Created: ${chalk.cyan(this.config.configDir)}`);
+      fs.mkdirSync(this.config.configDir)
+      this.log(`Created: ${chalk.cyan(this.config.configDir)}`)
     }
     this.log(
       `${chalk.greenBright(
-        "Success!"
+        'Success!'
       )} Your configuration directory has been setup!`
-    );
+    )
 
     // Setup the user's Twitter API credentials
     this.log(`\nWe're going to setup the Twitter API credentials now!
       \nGet your keys from ${chalk.cyan(
-        "https://developer.twitter.com/apps"
-      )}\nWe strongly recommend using a seperate app for these credentials.\n`);
-    const configAnswers: any = await prompt(Config.configQuestions);
+        'https://developer.twitter.com/apps'
+      )}\nWe strongly recommend using a seperate app for these credentials.\n`)
+    const configAnswers: any = await prompt(Config.configQuestions)
 
     // Overwriting usercredentials
     if (fs.existsSync(`${this.config.configDir}/creds.json`)) {
       this.log(
         `\n${chalk.yellow(
-          "Warning:"
+          'Warning:'
         )} Found existing credentials. Continuing with setup will overwrite these.`
-      );
-      const { confirm } = await prompt({
-        type: "confirm",
-        name: "confirm",
-        message: "Continue?"
-      });
-      if (!confirm) this.exit(0);
+      )
+      const {confirm} = await prompt({
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Continue?'
+      })
+      if (!confirm) this.exit(0)
     }
 
     // Save the credentials
-    this.log("\nCreating credentials file...");
-    const credsFile: string = `${this.config.configDir}${
-      this.config.windows ? "\\" : "/"
-    }creds.json`;
+    this.log('\nCreating credentials file...')
+    const credsFile = `${this.config.configDir}${
+      this.config.windows ? '\\' : '/'
+    }creds.json`
     try {
-      fs.writeFileSync(credsFile, JSON.stringify(configAnswers));
+      fs.writeFileSync(credsFile, JSON.stringify(configAnswers))
       this.log(
         `${chalk.greenBright(
-          "Success!"
+          'Success!'
         )} Your configuration file has been created!`
-      );
-      this.log(chalk.cyan(credsFile));
+      )
+      this.log(chalk.cyan(credsFile))
     } catch {
       this.error(
         `Something went wrong with creating your file.\n${chalk.red(credsFile)}`
-      );
-      this.log(chalk.red(credsFile));
+      )
+      this.log(chalk.red(credsFile))
     }
 
     // Woot!
     this.log(
-      `\nYou can now start using ${chalk.cyan("twitter-cl")}! 
-      \nGo ahead and try the command: ${chalk.cyan.underline("tcl feed")}`
-    );
+      `\nYou can now start using ${chalk.cyan('twitter-cl')}!
+      \nGo ahead and try the command: ${chalk.cyan.underline('tcl feed')}`
+    )
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,11 @@
-// Function to check if file exists
+import * as fs from 'fs'
+import * as Twitter from 'twitter'
+
+// Check if credentials exist
+export const client = (configPath: string): object => {
+  if (fs.existsSync(`${configPath}/creds.json`)) {
+    const creds = fs.readFileSync(`${configPath}/creds.json`)
+    return new Twitter(creds)
+  }
+  throw new Error('Invalid Credentials')
+}
