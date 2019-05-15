e56ba6c9f59d42fe8c9b75d19e72b3574f38ec10
Leander Rodrigues <me@leander.xyz>
2019-05-14 20:26:51 -0400

    ✨ Add timeline feature!

--- src/commands/feed.ts
+++ src/commands/feed.ts
@@ -6,17 +6,28 @@ import {tclClient} from '../tcl-client'
 export default class Feed extends Command {
   static description = 'Get the latest tweets in your timeline'
   static flags = {
-    help: flags.help({char: 'h'})
+    help: flags.help({char: 'h'}),
+    count: flags.integer({
+      char: 'c',
+      description: 'Limit tweets to the specified number',
+      default: 30
+    })
   }
 
   async run() {
     const {flags} = this.parse(Feed)
     const T = tclClient(this.config.configDir)
-
-    const stream = T.stream('statuses/filter')
-    stream.on('error', error => this.error(error))
-    stream.on('data', data => {
-      this.log(data)
+    const feed = await T.get('statuses/home_timeline', {
+      count: flags.count || 30
+    })
+    feed.map(({id_str, text, user}) => {
+      this.log(
+        `\n${chalk.cyan(
+          `${user.name} (@${user.screen_name})`
+        )}:\n${text}\n${chalk.grey(
+          `https://twitter.com/${user.screen_name}/status/${id_str}`
+        )}`
+      )
     })
   }
 }
